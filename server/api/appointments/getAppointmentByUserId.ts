4cf7aed443a081e89f8d744f1a7a1fe5ca3404f4
LinhHoang <hoanglinh9955@gmail.com>
2024-05-14 01:21:18 +0700

    add Appointments

---
+++ server/api/appointments/getAppointmentByUserId.ts
@@ -0,0 +1,17 @@
+export default defineEventHandler(async (event) => {
+    try {
+        const query = getQuery(event)
+        
+    // const { user_id } = await readBody(event);
+        const results = event.context.cloudflare.env.DB.prepare(
+        'SELECT * FROM `appointments` WHERE user_id = ? '
+      ).bind(query.user_id).all()
+         //run return success: true or false and meta
+      return results;
+    } catch (e: any) {
+      console.error({
+        message: e.message
+      });
+    }
+  })
+  
--- server/api/appointments/getAppointments.ts
+++ server/api/appointments/getAppointments.ts
@@ -1,9 +1,9 @@
 export default defineEventHandler(async (event) => {
     try {
-        const query = getQuery(event)
+        // const query = getQuery(event)
         const results = event.context.cloudflare.env.DB.prepare(
-        'SELECT * FROM `appointments` WHERE doctor_id = ? '
-      ).bind(query.user_id).all()
+        'SELECT * FROM `appointments`'
+      ).all()
          //run return success: true or false and meta
       return results;
     } catch (e: any) {
